3410a35fd47b977db383ada4dd350d7ba3a88b65
Vladimir Semenkov <Uladzimir_Semiankou@epam.com>
2017-01-17 17:14:18 +0300

    Update MainServlet.java

--- src/main/java/com/mcharopkin/server/MainServlet.java
+++ src/main/java/com/mcharopkin/server/MainServlet.java
@@ -30,11 +30,6 @@ public class MainServlet extends HttpServlet {
 		resp.addHeader("Access-Control-Allow-Origin", "192.168.56.101:5080");
 		resp.addHeader("Access-Control-Allow_Methods", "*");
 		resp.setCharacterEncoding("UTF-8");
-		try {
-			System.out.println("hui");
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
 	}
 	
 	@Override
